4ea87f9749dd45443d3e5229b6f86952135883d5
akino-programmer <noguchi_a7@yahoo.co.jp>
2020-06-20 18:28:25 +0900

    countUp and Down function.

--- Swift5CountUp1/ViewController.swift
+++ Swift5CountUp1/ViewController.swift
@@ -10,11 +10,70 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var countLabel: UILabel!
+    
+    var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      
+        //ラベルの文字を0と表示したい
+        countLabel.text = "0"
+        
+        
     }
 
+    
+    
+    @IBAction func plus(_ sender: Any) {
+          //カウントアップさせたい
+        count = count + 1
+        
+        
+        
+        //ラベルに文字を反映（数字）
+        countLabel.text = String(count)
+        
+        
+        //色を10以上になったら黄色にしたい
+        if count >= 10{
+            changeTextColor()
+        }
+    }
+    
+    
+    @IBAction func minus(_ sender: Any) {
+           //カウントダウンさせたい
+         count = count - 1
+        
+        
+        //ラベルに文字を反映（数字）
+        countLabel.text = String(count)
+        
+     
+               
+               
+        //色を0以下になったら白にしたい
+        if count <= 0{
+                   resetColor()
+               }
+    }
+    
+    func changeTextColor(){
+        
+        countLabel.textColor = .yellow
+        
+    }
+    
+    
+    func resetColor(){
+        
+        countLabel.textColor = .white
+        
+    }
+    
+    
 
 }
 
